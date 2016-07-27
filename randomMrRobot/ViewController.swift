9cd804b0a6a6a9e7c302a555a54d351da9d3d016
Entgoddess <paige.walters92@gmail.com>
2016-07-27 16:20:08 -0500

    starting of quiz functunality

--- randomMrRobot/ViewController.swift
+++ randomMrRobot/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let quotes = ["We are in a war, and we are on the losing side of it.\nWe are on our knees with guns to our heads, and they are picking us off one by one.\n-Darlene", "How do I take off a mask when it stops being a mask?\nWhen it's as much a part of me as me?\n-Elliot", "Control is an illusion.", "Why this mask?\nIt's a bit silly, isn't it?\n-Tyrell", "In that moment, it's just you and absolute power.\nNothing else. That moment stayed with me.\nI thought I'd feel guilty for being a murderer but... I don't.\nI feel wonder.\n-Tyrell", "Corporate greed is a trickle down desire\nthat reaches even the bottom of the food chain.\n-Darlene"]
+    let quotes = ["We are in a war, and we are on the losing side of it.\nWe are on our knees with guns to our heads, and they are picking us off one by one.", "How do I take off a mask when it stops being a mask?\nWhen it's as much a part of me as me?"]
 
     @IBOutlet weak var backgroundImage: UIImageView!
     
     @IBOutlet weak var quoteLabel: UILabel!
+
+    var correctAnswers = "0"
+    var usersAnswer = "Darlene"
+    var stringAnswer = "Darlene"
+    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,20 +33,98 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func randomBtn(sender: AnyObject) {
-        setupView()
     
-    }
     
-
-   func randomQuote() -> String {
+    func randomQuote() -> String {
         let idx = Int(arc4random_uniform(UInt32(quotes.count)))
         return quotes[idx]
     }
     
     func setupView() {
         quoteLabel.text = randomQuote()
+        answerIndex()
+    }
+    
+    func correctAnswer() {
+            switch correctAnswers {
+            case "0":
+                stringAnswer = "Darlene"
+                print("\(correctAnswers) is Darlene")
+            case "1":
+                stringAnswer = "Elliot"
+                print("\(correctAnswers) is Elliot")
+            default:
+                print("\(correctAnswers) that is not the answer")
+            }
+        
+
+        comparingAnswers()
+    }
+    
+    func comparingAnswers(){
+        if usersAnswer == stringAnswer {
+            let alertController = UIAlertController(title: "Correct", message: "You're freaking awesome!", preferredStyle: .Alert)
+            
+            let cancelAction = UIAlertAction(title: "Next", style: .Cancel) { (action) in
+                self.setupView()
+                self.answerIndex()
+            }
+            alertController.addAction(cancelAction)
+            
+            
+            self.presentViewController(alertController, animated: true) {
+                // ...
+            }
+
+            print("YOU ARE CORRECT! :)")
+        } else {
+            print("Try Again :(")
+            
+            let alertController = UIAlertController(title: "Incorrect", message: "Better luck next time, buddy!", preferredStyle: .Alert)
+            
+            let cancelAction = UIAlertAction(title: "Next", style: .Cancel) { (action) in
+                self.setupView()
+                self.answerIndex()
+            }
+            alertController.addAction(cancelAction)
+            
+      
+            self.presentViewController(alertController, animated: true) {
+                // ...
+            }
+        }
+    }
+    func answerIndex() {
+        let answerIndex = quotes.indexOf(quoteLabel.text!)!
+        correctAnswers = "\(answerIndex)"
+        print(answerIndex)
+    }
+
+    @IBAction func randomBtn(sender: AnyObject) {
+       setupView()
+    }
+    
+    @IBAction func buttonOne(sender: AnyObject) {
+        print(sender.currentTitle!!)
+        usersAnswer = sender.currentTitle!!
+        correctAnswer()
+        
+    }
+    
+    @IBAction func buttonTwo(sender: AnyObject) {
+        print(sender.currentTitle!!)
+        usersAnswer = sender.currentTitle!!
+        correctAnswer()
+    }
+    
+    @IBAction func buttonThree(sender: AnyObject) {
+        print(sender.currentTitle!!)
+        usersAnswer = sender.currentTitle!!
+        correctAnswer()
+    }
+    @IBAction func buttonFour(sender: AnyObject) {
+        print(sender.currentTitle!!)
+        usersAnswer = sender.currentTitle!!
+        correctAnswer()
     }
 }
-
