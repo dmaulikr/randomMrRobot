cd3dacd79efec9c5cd574ca3abf0c148c28f3b67
Entgoddess <paige.walters92@gmail.com>
2016-08-09 12:44:13 -0500

    Trying out a dictionary, because why not?

--- randomMrRobot/ViewController.swift
+++ randomMrRobot/ViewController.swift
@@ -10,7 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let quotes: Set<String> = ["We are in a war, and we are on the losing side of it.\nWe are on our knees with guns to our heads, and they are picking us off one by one.", "How do I take off a mask when it stops being a mask?\nWhen it's as much a part of me as me?", "Control is an illusion.", "Why this mask?\nIt's a bit silly, isn't it?", "In that moment, it's just you and absolute power.\nNothing else. That moment stayed with me.\nI thought I'd feel guilty for being a murderer but... I don't.\nI feel wonder.", "Corporate greed is a trickle down desire\nthat reaches even the bottom of the food chain."]
+    let quotes: [String: String] = [
+    "Darlene_1" : "You wanna know who’s crazy? Those wall street psychos.",
+    "Darlene_2" : "Jesus, I don’t normally say this, but I have got to Instagram this. People need to know this movie exists.",
+    "Tyrell_1" : "I think about you a lot, Elliot. I think about that night. When we became gods.",
+    "Ray_1"    :    "Control is about as real as a unicorn taking a leak at the end of a double rainbow.",
+    "Elliot_1" : "If I don’t listen to my imaginary friend, why the fuck should I listen to yours?",
+    "Joanna_1" : "Killing a man instantly robs him of explanation. He has no time to process his final moments. Now, even though he was paralyzed, his mind was still able to understand why his life was ending. We let him die with answers. Otherwise, we’re nothing but ruthless murderers.",
+    "Dom_1" : "You’ve surrounded yourself with a constant reminder of mortality.",
+    "Elliot_2" : "I live for this shit.",
+    "Krista_1" : "Annihilation is not the way.",
+    "Elliot_3" : "He’s gone. He’s gone. He’s gone.",
+    "Elliot_4" : "I will not be owned",
+    "Elliot_5" : "I’m late for my church group. Peace.",
+    "Angela_1" : "I have value, and even though you don’t see it, they do.",
+    "Darlene_3" : "We are in a war, and we are on the losing side of it. We are on our knees with guns to our heads, and they are picking us off one by one.",
+    "Elliot_6" : "How do I take off a mask when it stops being a mask? When it’s as much a part of me as me?",
+    "MrRobot_1" : "Could you live with yourself knowing that you framed Gideon?",
+    "MrRobot_2" : "Control is an illusion.",
+    "Tyrell_2" : "It’s almost as if somethings come alive.",
+    "Tyrell_3" : "Why this mask? It’s a bit silly, isn’t it?",
+    "MrRobot_3" : "Is any of it real? I mean, look at this. Look at it! A world built on fantasy. Synthetic emotions in the form of pills. Psychological warfare in the form of advertising. Mind-altering chemicals in the form of…food! Brainwashing seminars in the form of media. Controlled isolated bubbles in the form of social networks. Real? You want to talk about reality? We haven’t lived in anything remotely close to it since the turn of the century. We turned if off, took out the batteries, snacked on a bad of GMOs while we tossed the remnants in the ever-expanding dumpster of the human condition. We live in branded houses trademarked by corporations built on bipolar numbers jumping up and down on digital displays, hypnotizing us into the biggest slumber mankind has ever seen. You have to dig pretty deep, kiddo, before you can find anything real. We live in a  kingdom of bullshit. A kingdom you’ve lived in for far too long. So don’t tell me about not being real. I’m no less real than the fucking beef patty in your Big Mac.",
+    "MrRobot_4" : "You’re losing it, kiddo. I’m only supposed to be your prophet. You’re supposed to be my God."
+    ]
 
     @IBOutlet weak var backgroundImage: UIImageView!
     
